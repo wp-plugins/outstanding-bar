af7056de2765703419409a7f539f9a16a50ccb12
wearecontrast <wearecontrast@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-15 20:04:50 +0000

    Tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/outstanding-bar/tags@1113404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0.1/OutstandingBarOptions.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Contrast;
+
+/**
+ * Description of OutstandingBarOptions
+ *
+ * @author Mike
+ */
+class OutstandingBarOptions {
+
+    private $optionGroup = 'outstandingBarPage';
+    private $optionName = 'outstandingBar_settings';
+    private $fieldPrefix = 'outstandingBar_';
+    private $renderSuffix = '_render';
+
+    public function getOptionGroup() {
+        return $this->optionGroup;
+    }
+
+    public function getOptionName() {
+        return $this->optionName;
+    }
+
+    public function getSectionName($name) {
+        return $this->optionGroup . '_' . $name . 'Section';
+    }
+
+    public function getFieldName($name) {
+        return $this->fieldPrefix . $name;
+    }
+
+    public function getRenderName($name) {
+        return $this->fieldPrefix . $name . $this->renderSuffix;
+    }
+
+    public function getFormElementName($name) {
+        return $this->optionName . '[' . $this->getFieldName($name) . ']';
+    }
+
+    public function getOption($name, $default='') {
+        $options = get_option($this->optionName);
+        $optionName = $this->getFieldName($name);
+        return (array_key_exists($optionName, $options)) ? $options[$optionName] : $default;
+    }
+
+    public function addSettingsSection($sectionName, $displayText, $fields) {
+        add_settings_section(
+                $this->getSectionName($sectionName)
+                , $this->_i18n($displayText)
+                , ''
+                , $this->getOptionGroup()
+        );
+        foreach($fields as $field){
+            $this->addSettingsField($field['name'], $field['text'], $sectionName);
+        }
+    }
+
+    private function _i18n($text, $domain = 'wordpress') {
+        return __($text, $domain);
+    }
+
+    public function addSettingsField($fieldName, $displayText, $sectionName) {
+        add_settings_field(
+                $this->getFieldName($fieldName)
+                , $this->_i18n($displayText)
+                , $this->getRenderName($fieldName)
+                , $this->getOptionGroup()
+                , $this->getSectionName($sectionName)
+        );
+    }
+    
+    public function outputFormElement($name, $type, $options=array()){
+        switch($type){
+            case 'text': $this->_outputTextElement($name, $options); break;
+            case 'checkbox': $this->_outputCheckboxElement($name, $options); break;
+            case 'select': $this->_outputSelectElement($name, $options); break;
+        }
+    }
+    
+    private function _outputTextElement($name, $options){
+        ?>
+        <input type="text" 
+               id="<?php echo $name; ?>"
+               name="<?php echo $this->getFormElementName($name); ?>" 
+               value="<?php echo $this->getOption($name, $this->_getDefaultFromOptions($options)); ?>">
+        <?php
+    }
+
+    private function _outputCheckboxElement($name, $options) {
+        ?>
+        <input type="checkbox"
+               id="<?php echo $name; ?>"
+               name="<?php echo $this->getFormElementName($name); ?>"
+               <?php checked($this->getOption($name, $this->_getDefaultFromOptions($options, '0')), 1); ?> 
+               value='1'>
+        <?php
+    }
+    
+    private function _outputSelectElement($name, $options){
+        ?>
+        <select id="<?php echo $name; ?>" name="<?php echo $this->getFormElementName($name); ?>">
+            <?php foreach($options['options'] as $option){ ?>
+                <option value="<?php echo $option['value']; ?>" 
+                    <?php selected($option['value'], $this->getOption($name, $this->_getDefaultFromOptions($options, 'helvetica'))); ?>>
+                        <?php echo $option['text']; ?>
+                </option>
+            <?php } ?>
+        </select>
+        <?php
+    }
+    
+    private function _getDefaultFromOptions($options, $default=''){
+        return (array_key_exists('default', $options)) ? $options['default'] : $default;
+    }
+
+}
---
+++ 1.0.1/options.php
@@ -0,0 +1,148 @@
+<?php
+require_once 'Mailchimp.php';
+require_once 'OutstandingBarOptions.php';
+
+add_action('admin_menu', 'outstandingBar_add_admin_menu');
+add_action('admin_init', 'outstandingBar_settings_init');
+
+function outstandingBar_add_admin_menu() {
+    add_options_page('Outstanding Bar', 'Outstanding Bar', 'manage_options', 'outstanding-bar', 'outstandingBar_options_page');
+}
+
+function outstandingBar_settings_init() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    register_setting($OBOptions->getOptionGroup(), $OBOptions->getOptionName());
+    outstandingBar_addMailchimpSection($OBOptions);
+    outstandingBar_addDisplaySection($OBOptions);
+}
+
+function outstandingBar_addMailchimpSection($OBOptions) {
+    $fields = array(
+        array('name' => 'apiKey', 'text' => 'Mailchimp API Key')
+        , array('name' => 'list', 'text' => 'Mailchimp List')
+    );
+    $OBOptions->addSettingsSection('mailchimp', 'MailChimp Settings', $fields);
+}
+
+function outstandingBar_addDisplaySection($OBOptions) {
+    $fields = array(
+        array('name' => 'isActive', 'text' => 'Show Outstanding Bar')
+        , array('name' => 'mainText', 'text' => 'Main Text')
+        , array('name' => 'signupButton', 'text' => 'Signup Button')
+        , array('name' => 'hideButton', 'text' => 'Hide Button')
+        , array('name' => 'mainColour', 'text' => 'Main Colour')
+        , array('name' => 'accentColour', 'text' => 'Accent Colour')
+        , array('name' => 'textColour', 'text' => 'Text Colour')
+        , array('name' => 'displayStyle', 'text' => 'Display Style')
+    );
+    $OBOptions->addSettingsSection('display', 'Display Settings', $fields);
+}
+
+function outstandingBar_apiKey_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('apiKey', 'text');
+}
+
+function outstandingBar_list_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $options = outstandingBar_getMailChimpLists();
+    ?>
+    <select id="list" name="<?php echo $OBOptions->getFormElementName('list'); ?>">
+        <?php foreach ($options as $option) { ?>
+            <option value="<?php echo $option['value']; ?>" <?php selected($OBOptions->getOption('list'), $option['value']); ?>><?php echo $option['text']; ?></option>
+        <?php } ?>
+    </select>
+    <?php
+}
+
+function outstandingBar_getMailChimpLists(){
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $options = array(
+        array('value' => '', 'text' => 'Please Select')
+    );
+    
+    $apiKey = $OBOptions->getOption('apiKey');
+    if (outstandingBar_isValidApiKey($apiKey)) {
+        $mc = new \Drewm\MailChimp($apiKey);
+        $lists = $mc->call('lists/list');
+        if ($lists['total'] > 0) {
+            foreach ($lists['data'] as $list) {
+                $options[] = array('value' => $list['id'], 'text' => $list['name'] . ' (' . $list['stats']['member_count'] . ')');
+            }
+        }
+    } else {
+        $options = array(array('value' => '', 'text' => 'Invalid API Key'));
+    }
+    return $options;
+}
+
+function outstandingBar_isValidApiKey($apiKey){
+     if($apiKey !== '' && preg_match('/^[0-9a-f]{32}-us([1-9]|10)$/', $apiKey) === 1){
+        $mc = new \Drewm\MailChimp($apiKey);
+        $ping = $mc->call('helper/ping');
+        if(array_key_exists('msg', $ping) && $ping['msg'] === "Everything's Chimpy!"){
+            return true;
+        }
+     }
+     return false;
+}
+
+function outstandingBar_isActive_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('isActive', 'checkbox');
+}
+
+function outstandingBar_mainText_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('mainText', 'text', array('default' => 'Enter your email to signup to our newsletter'));
+}
+
+function outstandingBar_signupButton_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('signupButton', 'text', array('default' => 'sign up'));
+}
+
+function outstandingBar_hideButton_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('hideButton', 'text', array('default' => 'hide'));
+}
+
+function outstandingBar_mainColour_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('mainColour', 'text', array('default' => '#333333'));
+}
+
+function outstandingBar_accentColour_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('accentColour', 'text', array('default' => '#fff000'));
+}
+
+function outstandingBar_textColour_render() {
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('textColour', 'text', array('default' => '#ffffff'));
+}
+
+function outstandingBar_displayStyle_render() {
+    $options = array(
+        array('value' => 'Always', 'text' => 'Always')
+        ,array('value' => 'OnScrollUp', 'text' => 'On Scroll Up')
+        ,array('value' => '50Percent', 'text' => 'Scroll >50%')
+    );
+    $OBOptions = new \Contrast\OutstandingBarOptions();
+    $OBOptions->outputFormElement('displayStyle', 'select', array('options' => $options));
+}
+
+function outstandingBar_options_page() {
+    ?>
+    <form action='options.php' method='post'>
+        <h2>Outstanding Bar Settings</h2>
+
+        <?php
+        settings_fields('outstandingBarPage');
+        do_settings_sections('outstandingBarPage');
+        submit_button();
+        ?>
+    </form>
+    <?php
+}
+?>
